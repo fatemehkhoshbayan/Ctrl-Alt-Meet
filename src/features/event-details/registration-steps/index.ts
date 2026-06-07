0718caf4fdeb153c0c44b9f89a48d292c7e36091
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-06-07 17:23:45 -0400

    refactor: remove attendee-related components and registration steps for a streamlined booking process

--- src/features/event-details/registration-steps/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AttendeeDetailsStep } from './AttendeeDetailsStep';
-export { default as TicketSelectionStep } from './TicketSelectionStep';
-export { default as BookingSummaryStep } from './BookingSummaryStep';
-export { default as SuccessView } from './SuccessView';
