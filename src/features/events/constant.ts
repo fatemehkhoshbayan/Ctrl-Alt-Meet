39322850e24408d3be53eb24a8e296e258146642
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-06-07 21:48:55 -0400

    feat: add upcoming date range option and implement price range slider in SideBar component

--- src/features/events/constant.ts
+++ src/features/events/constant.ts
@@ -7,6 +7,7 @@ export const SORT_OPTIONS = [
 
 export const DATE_RANGE_OPTIONS = [
   { value: 'anytime', label: 'Anytime' },
+  { value: 'upcoming', label: 'Upcoming' },
   { value: 'today', label: 'Today' },
   { value: 'this-week', label: 'This Week' },
   { value: 'this-month', label: 'This Month' },
